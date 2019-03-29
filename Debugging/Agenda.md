e7411c598d9e49e7314fbd3fda46fbbf949fe7a2
kvprasoon <prasoonkarunan5@hotmail.com>
2019-03-29 22:23:02 +0530

    demo files

---
+++ Debugging/Agenda.md
@@ -0,0 +1,68 @@
+
+
+# $MySelf
+
+    Just another PowerShell fan
+
+  >- Prasoon Karunan V
+
+  >- Sr. Software Engineer @ Philips
+
+  >- @prasoonkaruanan
+
+  >- powershell.org/forums/users/prasoon-karunan-v/
+
+  >- <a href="https://stackoverflow.com/users/4980370/prasoon-karunan-v"><img src="https://stackoverflow.com/users/flair/4980370.png"></a>
+
+  >- github.com/kvprasoon
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+
+## Agenda
+
+
+
+
+>### - [What is debugging ?](#What-is-debugging )
+
+>### - [Cmdlets enabling debugging in PowerShell.](#Cmdlets-enabling-debugging-in-PowerShell)
+
+>### - [Debugging scripts locally from command line.](#Debugging-scripts-locally-from-command-line.)
+
+>### - [Debugging PowerShell background job.](#Debugging-PowerShell-background-job.)
+
+>### - [Debugging in remote session.](#Debugging-in-remote-session.)
+
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+># What is debugging ?
+
+   - The process of identifying and removing errors from computer hardware or software.
+
+   -  In software development, debugging involves locating and correcting code errors in a computer program.
+
+   - Debugging is the process of detecting and removing of existing and potential errors (also called as ‘bugs’) in a software code that can cause it to behave unexpectedly or crash
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+># Cmdlets enabling debugging in PowerShell.
+
+   Debugging in PowerShell can be done via both UI(a code editor) and command line. The command line debuggin in PowerShell is acheived using below cmdlets.
+
+  - Disable-PSBreakpoint
+  - Enable-PSBreakpoint
+  - Get-PSBreakpoint
+  - Remove-PSBreakpoint
+  - Set-PSBreakpoint
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+># Debugging scripts locally from command line.
+
+      Demo
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+># Debugging PowerShell background job.
+
+      Demo
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+># Debugging in remote session.
+
+      Demo
---
+++ Debugging/UpgradepwshDebug.ps1
@@ -0,0 +1,139 @@
+#requires -RunAsAdministrator
+
+
+<#PSScriptInfo
+
+.VERSION 1.0.2
+
+.GUID 48fcb5e2-0e6a-4a93-9699-f44fc7156482
+
+.AUTHOR kvprasoon
+
+.COMPANYNAME PSBUG
+
+.COPYRIGHT kvprasoon
+
+.TAGS pwsh powershell upgrade
+
+.LICENSEURI
+
+.PROJECTURI https://github.com/kvprasoon/PwshUtils/blob/master/Utilities/UpdatePwsh.ps1
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+7/21/2018
+Added -Prerelease switch which option of installing Release or pre release version.
+
+7/20/2018
+Upgrade pwsh script support is currently available only for below Operating Systems.
+ - Windows
+#>
+
+
+
+
+
+
+
+<#
+
+.DESCRIPTION
+ Script to upgrade pwsh to the latest available release(Only for windows now)
+
+.EXAMPLE
+.\UpgarePwsh.ps1
+
+The above command will install the latest released version of PowerShell core.
+
+.EXAMPLE
+.\UpgarePwsh.ps1 -PreRelease
+
+The above command will install the latest pre release version of PowerShell core.
+
+#>
+
+Param(
+    # Use -PreRelease switch to install pre release version of PowerShell core.
+    [switch]$Prerelease,
+
+    [switch]$32Bit
+)
+function InstallForWindows{
+    Param(
+        $Release
+    )
+    $OutputPath = "$env:HOMEPATH\Downloads"
+
+    if( -not $32Bit.IsPresent ){
+        $FilterCriteria  = { ($_.Name -match '.msi') -and ($_.Name -match "x64") }
+    }
+    else{
+        $FilterCriteria  = { ($_.Name -match '.msi') -and ($_.Name -match "x86") }
+    }
+
+    Write-Host "Latest released packages are below"
+    $RequiredPackage = $Release | Where-Object $FilterCriteria
+
+    $DownloadPath = "$OutputPath\$($RequiredPackage.Name)"
+
+    Write-Host "Downloading latest release $($RequiredPackage.Name) "
+    Invoke-WebRequest $RequiredPackage.Browser_Download_Url -Out "$DownloadPath" -ErrorAction Stop
+
+
+    Write-Host "Installing $($RequiredPackage.Name)"
+@"
+msiexec /i $DownloadPath /quiet
+start pwsh
+"@ | Out-File -FilePath $env:Temp\UpdatePwsh.bat
+    Start-Process -FilePath $env:Temp\UpdatePwsh.bat -ErrorAction Stop
+
+    Write-Host -ForegroundColor Green "Have fun using Pwsh ..."
+
+
+}
+try{
+
+    $Script:pwsh    = 'pwsh'
+    $ReleaseUrl     = "https://api.github.com/repos/PowerShell/PowerShell/releases"
+    $MetadataUrl    = "https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json"
+    $CurrentVersion = $PSVersionTable.PSVersion -as [string]
+
+    Write-Host "Installed pwsh version is $CurrentVersion"
+    Write-Host "Fetching latest releases"
+
+    $ReleaseMetadata = Invoke-RestMethod -Uri $MetadataUrl -ErrorAction Stop
+    Wait-Debugger
+    If( $PreRelease.IsPresent ){
+        $ReleaseToDownload = $ReleaseMetadata.NextRelesaeTag
+    }
+    else{
+        $ReleaseToDownload = $ReleaseMetadata.RelesaeTag
+    }
+
+    $Releases = Invoke-RestMethod -Uri $ReleaseUrl
+    $LatestRelease = ($Releases | Where-Object -FilterScript { $_.Tag_Name -eq $ReleaseToDownload }).assets | Select-Object -Property Name,Browser_Download_Url -ErrorAction Stop
+
+    If( $LatestRelease.Name -like "*$CurrentVersion*" ){
+        Write-Host "Currenlty executing pwsh is with latest available version"
+    }
+    else{
+        InstallForWindows -Release $LatestRelease
+        Stop-Process -Name pwsh -Force -ErrorAction Stop
+    }
+
+}
+Catch{
+
+    Throw "pwsh update failed with error '$_'"
+}
+# Finally{
+#     Remove-Item -Path $env:Temp\UpdatePwsh.bat -Force -ErrorAction SilentlyContinue
+# }
